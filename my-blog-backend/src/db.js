1fed76f75adc8b9295ba6026b3229a2a091b65e2
Seema25dhami <seemadhamiwal2001@gmail.com>
2023-09-13 12:17:29 +0530

    new updates

--- my-blog-backend/src/db.js
+++ my-blog-backend/src/db.js
@@ -2,7 +2,7 @@
 import { MongoClient } from 'mongodb';
 let db;
 async function connectToDb(cb){
-    const client = new MongoClient(`mongodb+srv://node-server:9o8GroZu6zWHrQnA@cluster0.tqko0nh.mongodb.net/?retryWrites=true&w=majority`);
+    const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.tqko0nh.mongodb.net/?retryWrites=true&w=majority`);
     await client.connect();
 
 
